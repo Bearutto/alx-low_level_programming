e390a31f49505adbd89f3d0b0cd347f827e1e1fa
Bearutto <132428909+Bearutto@users.noreply.github.com>
2023-06-16 14:18:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main -A C program that prints using printf or puts
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -10,5 +10,5 @@
 int main(void)
 	write(2,
 			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	return (1);
 }
