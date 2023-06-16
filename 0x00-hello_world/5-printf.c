a9164adf84013046caf91477993c74c480c44d07
Bearutto <132428909+Bearutto@users.noreply.github.com>
2023-06-16 00:54:03 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h
+/**
+ * main - A C prorgam that prints statements usin printf and returns o
+ * return; o
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
