be25d3245c6ba11a61da1d42534626b3ad9f7cc0
Bearutto <132428909+Bearutto@users.noreply.github.com>
2023-06-17 23:52:09 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char b;
+
+	b = 'a';
+	while
+		(b <= 'z') {
+			putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
