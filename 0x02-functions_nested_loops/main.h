ecb7ec2d428a560105d16609613c5787385f3d9f
Bearutto <132428909+Bearutto@users.noreply.github.com>
2023-06-21 01:29:05 -0700

    header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,18 @@
 #ifndef main.h
 #define main.h
 
-0-putchar.c
-1-alphabet.c
-2-print_alphabet_x10.c
-3-islower.c
-4-isalpha.c
-5-sign.c
-6-abs.c
-7-print_last_digit.c
-8-24_hours.c
-9-times_table.c
-10-add.c
-11-print_to_98.c
-_putchar
+int _putchar(char k)
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
